f7e3eabb3d8c3f5a03dcdb835bd0864651830d4f
erichvorster <erichv@infoslips.com>
2022-10-07 10:54:43 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Carousel, { CarouselItem } from "./components/Carousel";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Carousel>
+        <CarouselItem>
+          {/* <div className="card">
+            <div className="card-icon">
+
+            </div>
+            <div className="card-content">
+              <h4>Card title</h4>
+              <p>Lorem ipsum dolor sit amet consectetur, </p>
+            </div>
+          </div> */}
+         item 1
+        </CarouselItem>
+        <CarouselItem>Item 2</CarouselItem>
+        <CarouselItem>Item 3</CarouselItem>
+      </Carousel>
     </div>
   );
 }
